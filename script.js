a322e4c0f2dc088af064e9ef9b8bd3c851c80e0b
igicruz <ialderete12@gmail.com>
2017-03-18 00:10:02 -0700

    add file
    add file

---
+++ script.js
@@ -0,0 +1,12 @@
+/*JS*/
+
+var arrayName = new Array("Name: Ignacio","Age: 41","Salary: $200","City: Heber City","State: UT")
+
+alert(arrayName [0]);
+alert(arrayName [1]);
+alert(arrayName [2]);
+alert(arrayName [3]);
+alert(arrayName [4]);
+
+	
+		
